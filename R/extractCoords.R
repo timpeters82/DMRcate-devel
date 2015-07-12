d0c8e586f5efe552de23f0d2b335b442d7976c51
Tim Peters <Tim.Peters@csiro.au>
2015-07-12 23:57:58 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 8241440afe0a1a7581f29c85afe0b648de1250b6
    
        DMRcate_1.5.61
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DMRcate@106018 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/extractCoords.R
+++ R/extractCoords.R
@@ -1,15 +1,14 @@
+#Author: tim.triche@usc.edu, April 29th, 2014
+
 extractCoords <- function(xx) 
 { 
   if(length(xx) > 1) {
-    res <- data.frame(t(sapply(as.character(xx), extractCoords)))
-    DataFrame(chrom=res$chrom, 
-              chromStart=as.numeric(as.character(res$chromStart)), 
-              chromEnd=as.numeric(as.character(res$chromEnd)))
+    t(sapply(xx, extractCoords))
   } else {
-    coords <- strsplit(as.character(xx), ':')[[1]]
+    coords <- strsplit(xx, ':')[[1]]
     chrom <- coords[1]
-    chromStart <- as.numeric(strsplit(coords[2], '-')[[1]][1])
-    chromEnd <- as.numeric(strsplit(coords[2], '-')[[1]][2])
+    chromStart <- strsplit(coords[2], '-')[[1]][1]
+    chromEnd <- strsplit(coords[2], '-')[[1]][2]
     return(c(chrom=chrom, chromStart=chromStart, chromEnd=chromEnd))
   }
 }
--- R/extractRanges.R
+++ R/extractRanges.R
@@ -1,31 +1,9 @@
-#' extract GRanges corresponding to DMRs from the results of a dmrcate run
-#' 
-#' @param   dmrcated  an object of class dmrcate.output
-#' @param   bySign    boolean, shall the DMRs be extracted by sign?  (FALSE)
-#' @param   atCutoff  a p-value cutoff desired for extraction, or NULL (NULL)
-#' 
-extractRanges <- function(dmrcated, bySign=FALSE, atCutoff=NULL, ...) {
-
-  if (is(dmrcated, "GRanges") || is(dmrcated, "GRangesList")) {
-    return(dmrcated)
-  } else if (!is(dmrcated, "dmrcate.output")) {
-    stop("Argument is not of class dmrcate.output; cannot process.")
-  } else {
-    dmrc <- DataFrame(cbind(extractCoords(dmrcated$results$hg19coord), 
-                            dmrcated$results))
-    dmrc$score <- dmrcated$results$maxbetafc 
-    if (!is.null(atCutoff)) dmrc <- dmrc[dmrc$pcutoff == atCutoff, ]
-    gr <- makeGRangesFromDataFrame(dmrc, keep.extra.columns=TRUE)
-    genome(gr) <- "hg19" ## nothing else is supported, yet :-/
-    if (bySign == FALSE) {
-      # GRanges
-      return(gr)
-    } else if (bySign == TRUE) {
-      # GRangesList
-      grl <- split(gr, sign(score(gr)))
-      names(grl) <- sub("1", "hyper", sub("-1", "hypo", names(grl)))
-      return(grl)
-    }
-  }
-
+extractRanges <- function(dmrcoutput) {
+  stopifnot(is(dmrcoutput, 'dmrcate.output'))
+  coords <- extractCoords(dmrcoutput$results$hg19coord)
+  coords <- cbind(coords, dmrcoutput$results[,c("gene_assoc", "group", "no.probes",
+                                                "minpval", "meanpval", "maxbetafc")])
+  coords$chromStart <- as.integer(as.character(coords$chromStart))
+  coords$chromEnd <- as.integer(as.character(coords$chromEnd))
+  makeGRangesFromDataFrame(coords, keep.extra.columns=TRUE)
 }
