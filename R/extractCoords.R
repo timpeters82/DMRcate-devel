640ef3876e59e335702d0732ef84ac5554da7f8a
Michael Buckley <michaelmikebuckley@gmail.com>
2017-03-15 15:17:54 +1100

    Faster version with no 'apply' calls.

--- R/extractCoords.R
+++ R/extractCoords.R
@@ -1,8 +1,14 @@
-#Thank you to Xavier Pastor from Bioconductor mailing list for this patch
-extractCoords <- function(xx)
-{
-    coords <- sapply(xx, strsplit, '[:-]')
-    coords <- as.data.frame(do.call(rbind, coords), stringsAsFactors=F)
-    colnames(coords) <- c('chrom', 'chromStart', 'chromEnd')
-    return(coords)
-}
+
+extractCoords <-
+  function(coords.M)
+  {
+    M <- length(coords.M)
+    A <- 3 # Should be three components
+    split <- "[:-]"
+    l <- strsplit(coords.M, split = split)
+    stopifnot(all(sapply(l, length) == A))
+    X.AM <- matrix(unlist(l), A, M)
+    df.MA <- data.frame(t(X.AM), stringsAsFactors = FALSE)
+    colnames(df.MA) <- c('chrom', 'chromStart', 'chromEnd')
+    df.MA
+  }
