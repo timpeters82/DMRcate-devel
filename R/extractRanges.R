844615c312adbccc8659cfbeb11244943c8018bc
Michael Buckley <michaelmikebuckley@gmail.com>
2017-03-17 07:57:26 +1100

    Unloop promoters part of extractRanges.

--- R/extractRanges.R
+++ R/extractRanges.R
@@ -1,4 +1,4 @@
-extractRanges <-function(dmrcoutput, genome=c("hg19", "hg38", "mm10")) 
+extractRanges <-function(dmrcoutput, genome=c("hg19", "hg38", "mm10"))
 {
   env <- new.env(parent=emptyenv())
   data(dmrcatedata, envir=env)
@@ -8,20 +8,21 @@ extractRanges <-function(dmrcoutput, genome=c("hg19", "hg38", "mm10"))
   coords$chromStart <- as.integer(as.character(coords$chromStart))
   coords$chromEnd <- as.integer(as.character(coords$chromEnd))
   ranges <- makeGRangesFromDataFrame(coords, keep.extra.columns = TRUE)
-  switch(genome, 
+  switch(genome,
     hg19={tx=env$tx.hg19},
     hg38={tx=env$tx.hg38},
     mm10={tx=env$tx.mm10}
   )
   promsidx <- as.data.frame(findOverlaps(ranges, promoters(tx, 2000, 2000)))
   proms <- tapply(promsidx$subjectHits, promsidx$queryHits, function(x) tx[x])
-  ranges$overlapping.promoters <- NA
-  promcount <- 1
-  for(i in 1:length(ranges)){
-    if(i %in% as.numeric(names(proms))){
-      ranges$overlapping.promoters[i] <- paste(proms[[promcount]]$tx_name, collapse=", ")
-      promcount <- promcount + 1
-    }
-  }
+
+  op.A <- sapply(proms, function(l) paste(l$tx_name, collapse= ", "))
+  name.A <- names(proms)
+  m.A <- as.numeric(name.A)
+  M <- max(m.A)
+  overlapping.promoters <- rep(NA_character_, M)
+  overlapping.promoters[m.A] <- op.A
+  ranges$overlapping.promoters <- overlapping.promoters
+
   ranges
 }
